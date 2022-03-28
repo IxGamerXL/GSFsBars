0cd79fbe83bbcdc004ffcb3b041f694249c9073f
IxGamerXL but creative <90928500+IxGamerXL@users.noreply.github.com>
2022-03-28 02:48:30 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,15 @@ bt.bars = [];
 bt.barid = 1;
 var barrow = 0;
 var cont, barcont, list, listupdate;
-var bedit_name, bedit_max, bedit_val, bedit_stepi, bedit_stepd;
+var bedit_name, bedit_max, bedit_val, bedit_pos, bedit_stepi, bedit_stepd;
 var bedit_color, bedit_colorr, bedit_colorg, bedit_colorb;
 var bdialog, bedit, editBar;
 
 // Static bar dimensions
 const BARSIZE_WIDTH = 250;
-const BARSIZE_HEIGHT = 45;
+const BARSIZE_HEIGHT = 32;
+
+const pu = () => Vars.player.unit();
 
 // Bar Protocol
 const createBar = (id) => new Bar(
@@ -19,7 +21,21 @@ const createBar = (id) => new Bar(
 		.replace(/@mv/g, bt.get(id).mv),
 	() => bt.get(id).color,
 	() => bt.get(id).v/bt.get(id).mv
-)
+);
+
+// Camera Cursor
+var drawingCCursor = false;
+function drawCameraCursor(){
+	if(!drawingCCursor) return;
+	let ccp = Core.camera.position;
+	
+	Lines.stroke(2);
+	Draw.color(Color.rgb(220,65,65));
+	Lines.line(ccp.x-32,ccp.y, ccp.x+32,ccp.y);
+	Lines.line(ccp.x,ccp.y-32, ccp.x,ccp.y+32);
+	Draw.reset();
+}
+Events.run(Trigger.draw, drawCameraCursor);
 
 function ui_init(){
 	// Bar Dialog
@@ -38,6 +54,7 @@ function ui_init(){
 	// Bar Editor Dialog
 	bedit = new BaseDialog("GSF Bars - Edit");
 	bedit.addCloseButton();
+	bedit.cont.setTranslation(0, 175);
 	editBar = function(id){
 		let bar = bt.get(id);
 		bedit_name = bar.name;
@@ -45,6 +62,7 @@ function ui_init(){
 		bedit_colorr = bar.color.r;
 		bedit_colorg = bar.color.g;
 		bedit_colorb = bar.color.b;
+		bedit_pos = bar.pos;
 		bedit_stepi = bar.svi;
 		bedit_stepd = bar.svd;
 		bedit_val = bar.v;
@@ -80,7 +98,7 @@ function ui_init(){
 			st.row();
 			st.label(() => "Color: ");
 			st.table(cons(cui => {
-				cui.label(() => "R");
+				cui.label(() => "R ");
 				cui.slider(0, 1, .1, bedit_colorr, input => {
 					bedit_colorr = input;
 					bedit_color = Color.rgb(
@@ -91,7 +109,7 @@ function ui_init(){
 				});
 				
 				cui.row();
-				cui.label(() => "G");
+				cui.label(() => "G ");
 				cui.slider(0, 1, .1, bedit_colorg, input => {
 					bedit_colorg = input;
 					bedit_color = Color.rgb(
@@ -102,7 +120,7 @@ function ui_init(){
 				});
 				
 				cui.row();
-				cui.label(() => "B");
+				cui.label(() => "B ");
 				cui.slider(0, 1, .1, bedit_colorb, input => {
 					bedit_colorb = input;
 					bedit_color = Color.rgb(
@@ -112,6 +130,57 @@ function ui_init(){
 					);
 				});
 			}));
+			
+			if(bar.popped){
+				st.row();
+				st.label(() => "Position: ");
+				st.table(cons(pui => {
+					pui.label(() => "X ");
+					let fieldX = pui.field(bedit_pos.x, input => {
+						parseFloat(bedit_pos.set(input, bedit_pos.y));
+					}).get();
+					
+					pui.row();
+					pui.label(() => "Y ");
+					let fieldY = pui.field(bedit_pos.y, input => {
+						parseFloat(bedit_pos.set(bedit_pos.x, input));
+					}).get();
+					
+					pui.row();
+					pui.label(() => "XY ");
+					pui.button("Set", () => {
+						bedit.hide();
+						bdialog.hide();
+						cont.visible = false;
+						drawingCCursor = true;
+						let tempPTable = Core.scene.table();
+						tempPTable.setTranslation(0, -(Core.scene.height*.3));
+						let ccp = Core.camera.position;
+						tempPTable.button("Set", () => {
+							try{
+								bedit_pos.set(Math.floor(ccp.x/8),Math.floor(ccp.y/8));
+								fieldX.text = bedit_pos.x;
+								fieldY.text = bedit_pos.y;
+								tempPTable.clear();
+								tempPTable.visible = false;
+								drawingCCursor = false;
+								cont.visible = true;
+								bdialog.show();
+								bedit.show();
+							}catch(e){
+								// Notify the player that the mod is having a stroke
+								Vars.ui.showErrorMessage(e);
+								tempPTable.clear();
+								tempPTable.visible = false;
+								drawingCCursor = false;
+								cont.visible = true;
+							}
+						}).width(125).height(60);
+						tempPTable.row();
+						tempPTable.label(() => Math.floor(ccp.x/8)+", "+Math.floor(ccp.y/8))
+					}).width(100);
+				}));
+			}
 			
 			st.row();
 			st.label(() => "Maximum Value: ");
@@ -155,6 +224,12 @@ function ui_init(){
 			bui.button("[red]Delete Bar", () => {
 				bedit.hide();
 				bar.remove();
+			});
+			if(!bar.popped) let pob = bui.button("Popout", () => {
+				Vars.ui.showConfirm("Popout bar?", "You will gain new settings for it, but you cannot revert this action and must recreate it to go back into Listed form.", () => {
+					bedit.hide();
+					bar.popout();
+				});
 			});
 		}));
 		
@@ -201,20 +276,54 @@ function init(){
 			svi: .1,
 			svd: .1,
 			color: ev(color, Color.rgb(
-				.8*255,
-				.2*255,
-				.2*255
+				.9*255,
+				.3*255,
+				.3*255
 			)),
+			pos: new Vec2(0,0),
+			popped: false,
 			
 			// Functions
 			remove(){
 				let br = bt.get(id);
 				br.container.clear();
 				br.frame.clear();
+				if(br.popcont != null){
+					br.popcont.clear();
+					br.popcont.visible = false;
+				}
 				delete bt.bars[id];
+			},
+			popout(){
+				let binst = bt.get(id);
+				if(binst.popped) return;
+				
+				binst.container.clear();
+				binst.popcont = Core.scene.table();
+				let binp = binst.popcont;
+				let barRend = createBar(id);
+				assemble(binp, barRend, 1);
+				
+				let popfunc = () => {
+					try{
+						if(binst == null) return;
+						
+						let cc = Core.camera, ccp = cc.position, cs = Core.scene;
+						let daxis = (dimensionType) =>
+							cs[dimensionType]/cc[dimensionType];
+						let taxis = (axis) =>
+							(binst.pos[axis]*8 - ccp[axis]) * dim[axis];
+							
+						let dim = Vec2(daxis("width"), daxis("height"));
+						binst.popcont.setTranslation(taxis("x"), taxis("y"));
+					}catch(e){}
+				}
+				Events.on(Trigger, popfunc);
+				binst.popped = true;
 			},
 			
 			// UI
+			popcont: null,
 			container: new Table(),
 			frame: barframeroot,
 			bar: createBar(id)
@@ -224,26 +333,27 @@ function init(){
 		
 		let bcontainer = bard.container;
 		
-		bcontainer.defaults().pad(2);
-		bcontainer.button("<", ()=>{
-			bard.v -= bard.svd;
-			if(bard.v < 0) bard.v = 0;
-		}).width(40);
-		bcontainer.add(bard.bar)
-			.width(BARSIZE_WIDTH).height(BARSIZE_HEIGHT).pad(4);
-		bcontainer.button(">", ()=>{
-			bard.v += bard.svi;
-			if(bard.v > bard.mv) bard.v = bard.mv;
-		}).width(40);
+		function assemble(tabl, barElement, barw){
+			tabl.button("<", ()=>{
+				bard.v -= bard.svd;
+				if(bard.v < 0) bard.v = 0;
+			}).width(50).height(BARSIZE_HEIGHT+18).pad(2);
+			tabl.add(barElement)
+				.width(BARSIZE_WIDTH*barw).height(BARSIZE_HEIGHT).pad(4);
+			tabl.button(">", ()=>{
+				bard.v += bard.svi;
+				if(bard.v > bard.mv) bard.v = bard.mv;
+			}).width(50).height(BARSIZE_HEIGHT+18).pad(2);
+		}
+		assemble(bcontainer, bard.bar, 1);
 		barcont.add(bcontainer);
 		barcont.row();
 		
 		barframe.pad(5);
-		barframe.defaults().pad(4);
 		barframe.get().background(Tex.button);
 		let bar = createBar(id);
-		barframe.get().add(bar).width(BARSIZE_WIDTH).height(BARSIZE_HEIGHT);
-		barframe.get().button("Edit", () => editBar(id)).width(70);
+		assemble(barframe.get(), bar, 1.5);
+		barframe.get().button("Edit", () => editBar(id)).width(70).pad(6);
 		list.add(barframeroot);
 		
 		if(++barrow % 1 == 0) list.row();
@@ -251,10 +361,10 @@ function init(){
 		return bt.bars[id];
 	}
 	bt.get = (id) => bt.bars[id];
-	bt.clear = () => bt.bars.forEach(b => b.remove());
+	bt.clear = () => {for(bi in bt.bars) bt.get(bi).remove()};
 	bt.listAll = function(){
 		let m = "Bar IDs:\n";
-		bt.bars.forEach((bv,bi) => m += bi+"\n");
+		for(bi in bt.bars) m += bi+"\n";
 		Log.info(m);
 	}
 	
